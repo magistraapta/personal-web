a86a0f96ba30050de38b05ade1cd50446dc63b1d
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-10-25 15:59:03 +0800

    fix: add back button

--- src/routes/articles/$slug.tsx
+++ src/routes/articles/$slug.tsx
@@ -1,5 +1,5 @@
 import MarkdownRenderer from '@/components/Markdown'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/articles/$slug')({
   component: ArticlePage,
@@ -11,6 +11,7 @@ function ArticlePage() {
     const markdownPath = `/src/markdown/${slug}.md`
     return (
         <div className='max-w-4xl mx-auto p-6'>
+            <Link to="/articles" className='text-blue text-xl font-bold underline mb-6'>Go back</Link>
             <MarkdownRenderer filePath={markdownPath} />
         </div>
         
--- src/routes/articles/index.tsx
+++ src/routes/articles/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute} from '@tanstack/react-router'
 import { getArticles } from '@/utils/getArticle'
 import { ArticleCard } from '@/components/ArticleCard'
 
